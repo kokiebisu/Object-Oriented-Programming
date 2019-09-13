06ac31a17669629bc54fc7e103fbb221286bfc8e
kokiebisu <kenichikona@gmail.com>
2019-09-12 21:28:44 -0700

    formatted docstring for respective methods. Also defined the puporse of each class

--- Labs/Lab1/asteroid.py
+++ Labs/Lab1/asteroid.py
@@ -1,12 +1,24 @@
 import random
 import datetime
 
+"""
+This is a class that instantiates a Asteroid object. It
+will contain the properties: radius in meters, position and 
+velocity uses x, y, z indexes
+"""
+
 
 class Asteroid:
     id = 0
-    timestamp = 0
 
     def __init__(self, radius, position, velocity):
+        """
+        Initializes the properties of Asteroid
+        :param radius: A value of type int
+        :param position: A list consisted of 3 elements (x, y, z)
+        :param velocity: A tuple consisted of 3 elements (x, y, z)
+        """
+
         self._radius = radius
         self._position = position
         self._velocity = velocity
@@ -15,42 +27,92 @@ class Asteroid:
 
     @classmethod
     def addID(cls):
+        """
+        Adds a unique id as the property of the instance
+        :return: Unique id of type int
+        """
         cls.id = cls.id + 1
         return cls.id
 
     @classmethod
     def addTimestamp(cls):
+        """
+        Adds a time stamp property to the instance
+        :return: the timestamp that was created at the moment of the call of the method
+        """
         cls.timestamp = datetime.datetime.now()
         return cls.timestamp
 
     def getID(self):
+        """
+        Gets the ID
+        :return: an int value
+        """
         return self._id
 
     def getRadius(self):
+        """
+        Gets the radius in meters
+        :return: an int value
+        """
         return self._radius
 
     def getPosition(self):
+        """
+        Gets the position as a list of three elements x, y, z
+        :return: list of ints
+        """
         return self._position
 
     def getVelocity(self):
+        """
+        Gets the velocity as a tuple of three elements x, y, z
+        :return: tuple of ints
+        """
         return self._velocity
 
     def getTimestamp(self):
+        """
+        Gets the timestamp property of the instance
+        :return: a string value
+        """
         return self._timestamp
 
     def setRadius(self, radius):
+        """
+        Sets the radius of type int
+        :param radius: an int value
+        :return: an int value
+        """
         self._radius = radius
 
     def setPosition(self, position):
+        """
+        Sets the position of the Asteroid
+        :param position: an int value
+        :return: a list of ints
+        """
         self._position = position
 
     def setVelocity(self, velocity):
+        """
+        Sets the velocity of the Asteroid
+        :param velocity: an int value
+        :return: an int value
+        """
         self._velocity = velocity
 
     def move(self):
+        """
+        Moves the position of the Asteroid based on the velocity
+        """
         for index in range(len(self._position)):
             self._position[index] = self._position[index] + \
                 self._velocity[index]
 
     def __str__(self):
+        """
+        Prints a string on the console to display properties when initialized
+        :return: A string value
+        """
         return f"ID: {self.getID()}, Radius:{self.getRadius()}, Position: {self.getPosition()}, Velocity: {self.getVelocity()}, Timestamp: {self.getTimestamp()}"
--- Labs/Lab1/controller.py
+++ Labs/Lab1/controller.py
@@ -1,25 +1,45 @@
 from asteroid import Asteroid
 import random
 import time
+import datetime
+
+"""
+    A class that maintains a list of asteroid objects in the field. 
+    It includes methods that can create Asteroids with randomly generated 
+    radius, velocity, position and including it to a list.
+"""
 
 
 class Controller:
+
     def __init__(self):
+        """
+        Creates a list of Asteroids
+        """
         self.asteroids = []
 
     @staticmethod
     def createRandomAsteroid():
+        """
+        Instantiates Asteroids
+        :return: instantiated Asteroid object
+        """
         return Asteroid(Controller.generateRadius(), Controller.generatePosition(), Controller.generateVelocity())
-
-    def addAsteroid(self):
-        self.asteroids.append(Controller.createRandomAsteroid())
 
     @staticmethod
     def generateRadius():
+        """
+        Creates a randomly generated radius within the range of 1 and 4
+        :return: an int value
+        """
         return random.randint(1, 4)
 
     @staticmethod
     def generateVelocity():
+        """
+        Creates a randomly generated velocity within the range of 0 and 5
+        :return: a tuple of type ints
+        """
         x = random.randint(0, 5)
         y = random.randint(0, 5)
         z = random.randint(0, 5)
@@ -27,26 +47,43 @@ class Controller:
 
     @staticmethod
     def generatePosition():
+        """
+        Creates a randomly generated position with each element within the range of 0 and 100
+        :return: a list of type ints
+        """
         x = random.randint(0, 100)
         y = random.randint(0, 100)
         z = random.randint(0, 100)
         return [x, y, z]
 
+    def addAsteroid(self):
+        """
+        Adds the newly created asteroid to the class variable of Controller
+        """
+        self.asteroids.append(Controller.createRandomAsteroid())
+
     def simulate(self, seconds):
+        """
+        Simulates the movement of the asteroids. It will records the position, velocity, timestamp.
+        It 
+        """
         for x in range(0, seconds):
-            startTime = time.time()
+            start_time = datetime.datetime.now().microsecond / 10 ** 6
+            print(f"Start time: {start_time}")
             for y in range(len(self.asteroids)):
                 self.asteroids[x].move()
                 print(self.asteroids[x])
-            endTime = time.time()
-            time.sleep(1 - (endTime - startTime))
+                end_time = datetime.datetime.now().microsecond / 10 ** 6
+                print(f"End time: {end_time}")
+                time.sleep(1 - (end_time - start_time))
 
 
 def main():
     controller = Controller()
-    for x in range(1, 5):
+    for x in range(1, 101):
         controller.addAsteroid()
-    controller.simulate(2)
+    controller.simulate(3)
 
 
-main()
+if __name__ == '__main__':
+    main()
