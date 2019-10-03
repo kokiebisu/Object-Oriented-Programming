67c80102167ed1128cc01dcca6e06d7b3bd7b058
kokiebisu <a01056715@gmail.com>
2019-10-02 19:33:56 -0700

    added data.json

---
+++ Labs/Lab4/prompt.py
@@ -0,0 +1,61 @@
+class Prompt:
+    @staticmethod
+    def welcome():
+        """
+        Greets the users with a warm welcome
+        """
+        print("Welcome to the dictionary")
+
+    @staticmethod
+    def options():
+        """
+        Displays options to let users interact with the program
+        :return: an int
+        """
+        print("\nWhat would you like to do?"
+              "\n1. Search"
+              "\n2. Save File"
+              )
+        option_input = int(input())
+        return option_input
+
+    @staticmethod
+    def word():
+        """
+        Prompts the user for the word they want to search
+        :return: a string
+        """
+        print("What is the word? Type 'exitprogram' to quit.")
+        word_input = input().lower()
+        if word_input == 'exitprogram':
+            exit()
+        else:
+            return word_input
+
+    @staticmethod
+    def prompt_file_path():
+        """
+        Prompts the user for the filepath in which they want to load the dictionary from
+        :return: a string
+        """
+        print("Let us know which file you want to load!")
+        file_path = input()
+        return file_path
+
+    @staticmethod
+    def prompt_continue():
+        """
+        Prompts the user if they would like to continue using the program
+        :return: a boolean
+        """
+        print("Would you like to continue?"
+              "\n 1. Yes"
+              "\n 2. No"
+              )
+        continue_input = int(input())
+        if continue_input == 1:
+            return True
+        elif continue_input == 2:
+            return False
+        else:
+            print("Invalid Input")
