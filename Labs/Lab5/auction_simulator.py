56561be0eafdb9b9e05b09bf2fa0bf0d99ea79d2
kokiebisu <a01056715@gmail.com>
2019-10-09 19:15:08 -0700

    added docstring

--- Labs/Lab5/auction_simulator.py
+++ Labs/Lab5/auction_simulator.py
@@ -1,14 +1,16 @@
+""" A module that simulates an auction using Observer patterns"""
 import random
 
 
 class Auction:
     """
-    Holds the attribute of an Auction object. Sets up the auction for an item with the given
+    A class that holds the attribute of an Auction object. Sets up the auction for an item with the given
     starting price
     """
 
     def __init__(self, bidders, item, starting_price):
         """
+        Initializes the attributes for an auction instance
         :param bidders: a list of participating bidder objects
         :param item: a string representing the name of the item being auctioned
         :param starting_price: an int
@@ -18,6 +20,9 @@ class Auction:
         self._starting_price = starting_price
 
     def start(self):
+        """
+        Starts the auction
+        """
         print("Starting Auction!!!\n"
               "-------------------\n"
               f"Auctioning {self._item} starting at {self._starting_price}"
@@ -26,32 +31,51 @@ class Auction:
 
 class Auctioneer:
     """
-    Maintains a list of bidders and notifies when it accepts new bid. Auctioneers
+    A class that maintains a list of bidders and notifies when it accepts new bid. Auctioneers
     can accept new bids if it is greater than the highest current bid. When highest
     current bid updates, it will notify all the bidders except for the bidder that
     placed the new bid.
     """
 
     def __init__(self, highest_current_bid, highest_current_bidder):
+        """
+        Initializes the attributes for an auctioneer instance
+        :param highest_current_bid: a float
+        :param highest_current_bidder: a string
+        """
         self._highest_current_bid = highest_current_bid
         self._highest_current_bidder = highest_current_bidder
         self._bidders = []
 
-    def register_bidders(self, observer):
-        self._bidders.append(observer)
-
-    def __len__(self):
-        return len(self._bidders)
+    def register_bidders(self, bidder):
+        """
+        Registers the bidder to be observed
+        :param bidder: a bidder object
+        """
+        self._bidders.append(bidder)
 
     @property
     def highest_current_bid(self):
+        """
+        Gets the highest bid being promised
+        :return: a float
+        """
         return self._highest_current_bid
 
     @property
     def highest_current_bidder(self):
+        """
+        Gets the name of the person promising the highest bid
+        :return:
+        """
         return self._highest_current_bidder
 
     def update_info(self, bid, bidder):
+        """
+        Updates the highest bid and the highest bidder
+        :param bid: a float
+        :param bidder: a string
+        """
         if self._highest_current_bid != bid:
             self._highest_current_bid = bid
             self._highest_current_bidder = bidder
@@ -59,10 +83,17 @@ class Auctioneer:
 
     @property
     def bidders(self):
+        """
+        Gets all the bidders participating
+        :return: a list
+        """
         return self._bidders
 
     def notify_bidders(self):
-        # notify the observers
+        """
+        Notifies all the bidders there was an update on the highest bid
+        :return: a list
+        """
         for observer in self._bidders:
             updated_info = observer(self)
             if updated_info != None:
@@ -75,10 +106,8 @@ class Auctioneer:
 
 class Bidder:
     """
-    Observer
-    callable object -> object that can be called as a function
-    Class that holds the attributes of the people who place the bids
-    during the auction. Every time the auctioner accepts a bid, all the bidders
+    A class that holds the attributes of the people who place the bids
+    during the auction. Every time the auctioneer accepts a bid, all the bidders
     should be notified.
     """
 
@@ -112,8 +141,27 @@ class Bidder:
             else:
                 return None
 
+    @property
+    def name(self):
+        """
+        Gets the name of the bidder
+        :return: a string
+        """
+        return self._name
+
+    @property
+    def highest_bid(self):
+        """
+        Gets the highest bid of that person
+        :return: a float
+        """
+        return self._highest_bid
+
 
 def main():
+    """
+    Runs the program
+    """
     item_name = input("What is the name of the item?\n")
     starting_price = int(input("What is the starting price?\n"))
     auctioneer = Auctioneer(starting_price, "Starting Bid")
@@ -136,7 +184,7 @@ def main():
         print(f"Seems like no one was interested with the item_name")
 
     bidders_result = {
-        bidder._name: bidder._highest_bid for bidder in auctioneer._bidders}
+        bidder.name: bidder.highest_bid for bidder in auctioneer.bidders}
     for bidder, bid in bidders_result.items():
         print(f"{bidder}'s highest bid was ${bid}'")
 
