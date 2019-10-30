9f95722102fed45850c287fe0005d174643aab9d
kokiebisu <a01056715@gmail.com>
2019-10-30 16:53:50 -0700

    reworded instruction in readme.txt

--- Assignments/assignment3/Readme.txt
+++ Assignments/assignment3/Readme.txt
@@ -2,10 +2,14 @@ Run the controller.py as it is the starting point of the program.
 When a option appears, please select the number.
 Select the option of either 
 1. Add ingredients to the pizza
-You can choose from the following
+You can choose either choose from cheese or toppings
+
+If you select 'Cheese', you can choose from the following cheese:
 * Parmigiano Reggiano
 * Fresh Mozarella
 * Vegan Cheese
+
+If you select 'Topping', you can choose from the following topping:
 * Peppers
 * Pineapple
 * Mushrooms
@@ -13,4 +17,6 @@ You can choose from the following
 * Spinach
 * Pepperoni
 * Beyond Meat
-2. Finishing the transaction by getting the total cost.
+
+2. By selecting the 'pay' option, you can finish the transaction by getting
+the included ingredients and the total cost
--- Assignments/assignment3/test_controller.py
+++ Assignments/assignment3/test_controller.py
@@ -28,19 +28,25 @@ class TestController(TestCase):
         self.assertRaises(ValueError)
 
     def test_prompt_with_invalid_cheese_number(self):
+        """
+        Checks when a cheese that doesn't exist is entered in the add cheese screen
+        """
         controller = Controller()
         controller.add_cheese(4)
         self.assertRaises(ValueError)
 
     def test_prompt_with_invalid_topping_number(self):
         """
-        Checks when a ingredient that doesn't exist is entered in the add ingredient screen
+        Checks when a topping that doesn't exist is entered in the add topping screen
         """
         controller = Controller()
         controller.add_topping(8)
         self.assertRaises(ValueError)
 
     def test_prompt_with_valid_cheese_number(self):
+        """
+        Checks when a valid cheese number is entered in the add ingredient screen
+        """
         controller = Controller()
         controller.add_cheese(1)
         self.assertEqual(controller.pay(), "Pizza Ingredients: \nSignature Crust \nParmigiano Reggiano"
@@ -48,11 +54,12 @@ class TestController(TestCase):
 
     def test_prompt_with_valid_topping_number(self):
         """
-        Checks when a valid ingredient number is entered in the add ingredient screen
+        Checks when a valid topping number is entered in the add ingredient screen
         """
         controller = Controller()
         controller.add_topping(1)
-        self.assertEqual(controller.pay(), "Pizza Ingredients: \nSignature Crust \nPeppers \nTotal Cost: 6.49 \n")
+        self.assertEqual(controller.pay(
+        ), "Pizza Ingredients: \nSignature Crust \nPeppers \nTotal Cost: 6.49 \n")
 
     def test_prompt_with_multiple_ingredients(self):
         """
