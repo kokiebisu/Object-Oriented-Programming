209689f07a92949bb86bcaa826f46191ba26a6a1
kokiebisu <a01056715@gmail.com>
2019-11-24 22:10:37 -0800

    successfully retrieved arguments

--- Assignments/assignment5/pokedex.py
+++ Assignments/assignment5/pokedex.py
@@ -1,4 +1,5 @@
 import argparse
+import enum
 
 
 class PokedexMode(enum.Enum):
@@ -32,6 +33,7 @@ class Query():
         """
         self._mode = None
         self._data = None
+        self._input = None
         self._output = None
         self._result = None
         self._expand = None
@@ -40,7 +42,7 @@ class Query():
         """
         String representation of the object
         """
-        return f"Query[ Mode: {self._mode}, Data: {self._data}, Output: {self._output}]"
+        return f"Query[ Mode: {self._mode}, Data: {self._data}, Input: {self._input}, Output: {self._output}, Expand: {self._expand}]"
 
 
 def validate_mode(mode_name: str):
@@ -91,25 +93,42 @@ def accept_args() -> Query:
     parser.add_argument(
         "input", help="As input, the application can take in either a file name (Text file) or a name/id.")
     parser.add_argument(
-        "--expanded", help="When this flag is provided, ceratain attributes are expanded. That is the pokedex will do sub-queries to get more information about a partivular attribute. If this flag is not provided, the app will not get the extra information and just print what's provided. ")
+        "--expanded", help="When this flag is provided, ceratain attributes are expanded. That is the pokedex will do sub-queries to get more information about a partivular attribute. If this flag is not provided, the app will not get the extra information and just print what's provided. ", action='store_true')
     parser.add_argument("--output", default="print",
                         help="The location of output in which the query should be printed to. ")
 
     try:
         args = parser.parse_args()
+        print(args)
         query_ = Query()
-        query._mode = validate_mode(args.mode)
+        query_._mode = PokedexMode(args.mode)
+        query_._input = args.input
         if args.expanded:
-            self._expand = True
-        something = validate_input_source(args.input)
-        self._data = convert_to_data(something)
+            query_._expand = True
         if args.output:
-            output = validate_output_source(args.output)
-            self._output = output
+            query_._output = args.output
+        print(query_)
+        return query_
     except Exception as e:
         print(f"Error! Could not read arguments. \n{e}")
         quit()
 
 
+# def main(request_: Request) -> None:
+#     pass
+
+
 if __name__ == '__main__':
     query = accept_args()
+    # main(query)
+
+    # args = parser.parse_args()
+    # query_ = Query()
+    # query._mode = validate_mode(args.mode)
+    # if args.expanded:
+    #     self._expand = True
+    # something = validate_input_source(args.input)
+    # self._data = convert_to_data(something)
+    # if args.output:
+    #     output = validate_output_source(args.output)
+    #     self._output = output
